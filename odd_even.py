f8f8ffc8f5d66769f9f7c2e846cbb501b80fdac9
kidman007 <kidman007@gmail.com>
2017-06-29 14:49:12 -0500

    solved odd_even problem

--- odd_even.py
+++ odd_even.py
@@ -1,4 +1,7 @@
-# You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns N.
+# You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+# The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+# Write a method that takes the array as an argument and returns N.
+
 # For example:
 # [2, 4, 0, 100, 4, 11, 2602, 36]
 # Should return: 11
@@ -6,7 +9,30 @@
 # Should return: 160
 
 def find_outlier(integers):
-  bort = [x % 2 for x in integers]
-  if sum(bort) == 1
+  div_2_remain = [x % 2 for x in integers] # two things here: wrapping in [] outputs a list. Also, it's interesting that the for is formatted differently when on one line
 
-find_outlier([2, 4, 0, 100, 4, 11, 2602, 36])
+  even_odd = []   # create placeholder list
+  even_count = 0  # create placeholder variable
+
+  # figure out if it's an even or odd and make a list that ids even or odd better.
+  for x in div_2_remain:
+    if x == 0:
+        even_odd.append(0)
+        even_count += 1
+    else:
+        even_odd.append(1)
+        even_count += 0
+
+  # checks:
+  # print even_count
+  # print even_odd
+
+  # based on whether or not it's even or odd, it will index the number I flagged.
+  if even_count > 1:
+      position = even_odd.index(1)
+      return integers[position]
+  else:
+      position = even_odd.index(0)
+      return integers[position]
+
+# find_outlier([2, 4, 0, 100, 4, 11, 2602, 36])
