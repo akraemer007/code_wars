820f3fbc4b1ea530f5a7a9d36acd365570bb55ab
kidman007 <kidman007@gmail.com>
2017-07-04 17:12:23 -0500

    crushing it

--- exes_and_ohs.py
+++ exes_and_ohs.py
@@ -8,7 +8,24 @@
 # XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 # XO("zzoo") => false
 
+# original answer:
+# def xo(s):
+#     s = s.lower()
+#
+#     if s.count('o') == s.count('x'):
+#         return True
+#     else:
+#         return False
+
+# New Answer:
+# since it's a logical, I don't need to state the output.
 def xo(s):
-    return true
+    s = s.lower()
+    return s.count('o') == s.count('x')
 
 xo("ooxx")
+xo("ooxx")
+xo("xooxx")
+xo("ooxXm")
+xo("zpzpzpp")
+xo("zzoo")
