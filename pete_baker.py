a71acc4f7101c47868d7a91c27e107508476b803
kidman007 <kidman007@gmail.com>
2017-07-05 14:16:34 -0500

    crushed pete baker

---
+++ pete_baker.py
@@ -0,0 +1,43 @@
+# Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+#
+# Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+#
+# Examples:
+#
+# # must return 2
+# cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})
+# # must return 0
+# cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})
+
+# def cakes(recipe, available):
+#
+#     enough_for = []
+#
+#     for x in list(recipe.keys()):
+#         if x in available.keys():
+#             enough_for.append(int((available[x] / recipe[x])))
+#         else:
+#             return 0
+#     return min(enough_for)
+#
+#     # print(list(recipe.keys()))
+#     # print(enough_for)
+#
+#
+# cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200})
+# cakes({"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}, {"sugar": 500, "flour": 2000, "milk": 2000})
+
+# I liked this a lot. I got to learn about dictionaries. Above was my solution. After looking at other people's solutions, I'm realizing that I know nothing about dictionaries xD.
+# You can use dictionaries a lot more like lists like this (my attempt after taking a peak at the other answers)
+
+def cakes(recipe, available):
+    output = []
+    for x in recipe:
+        if x in available:
+            output.append(available.get(x,0)//recipe[x]) # use get to grab first output in dictionary
+        else:
+            return 0
+    print(min(output))
+
+cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200})
+cakes({"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}, {"sugar": 500, "flour": 2000, "milk": 2000})
